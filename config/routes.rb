6fbfa1ef3546916b5c066f4301b48bd13ae70bd3
Sakundo <sakundobusiness@gmail.com>
2023-11-29 21:54:18 -0300

    Modificando a posição das rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :customers
   root "welcome#index"
-
   get "inicio" => "welcome#index"
+
+  resources :customers
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
